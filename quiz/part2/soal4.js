e94b649dec1a8771b1d96b2207d94ca110fc108a
Z1nky <rakhaallea@gmail.com>
2025-03-04 16:25:51 +0700

    create file soal1-soal4.js di dalam quiz part 2

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,27 @@
+// Soal 4. Breaking Sentence (yet Again) and Count Each Length
+
+// Problem
+// Mirip seperti soal nomor 3, tapi tampilkan juga panjang kata masing-masingnya!
+
+
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
+
+let secondWord = word4.substring(4, 14);
+let thirdWord = word4.substring(15, 17);
+let fourthWord = word4.substring(18, 20);
+let fifthWord = word4.substring(21, 25);
+
+let secondWordLength = secondWord.length;
+let thirdWordLength = thirdWord.length;
+let fourthWordLength = fourthWord.length;
+let fifthWordLength = fifthWord.length;
+
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
